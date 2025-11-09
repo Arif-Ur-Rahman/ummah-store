7e8a08b0f053b3e9e40a8a4591b049d40520b8be
Arif Ur Rahman <arifurrahman.it.doc@gmail.com>
2025-11-09 22:31:17 +0600

    Products Catagory Updated

--- src/app/component/home/ProductList.tsx
+++ src/app/component/home/ProductList.tsx
@@ -117,117 +117,12 @@ const ProductList = () => {
           <button className="rounded-2xl w-max ring-1 ring-red-500 text-red-500 text-xs py-2 px-4 hover:bg-red-500 hover:text-white">Add to Cart</button>
         </Link>
 
-        <Link
-          href="/test"
-          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-        >
-          <div className="relative w-full h-80">
-            <Image
-              src="/products/product1.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-            />
-            <Image
-              src="/products/product2.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md "
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$29.99</span>
-          </div>
-          <div className="text-sm text-gray-500">My Description</div>
-          <button className="rounded-2xl w-max ring-1 ring-red-500 text-red-500 text-xs py-2 px-4 hover:bg-red-500 hover:text-white">Add to Cart</button>
-        </Link>
+       
+       
 
-        <Link
-          href="/test"
-          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-        >
-          <div className="relative w-full h-80">
-            <Image
-              src="/products/product1.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-            />
-            <Image
-              src="/products/product2.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md "
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$29.99</span>
-          </div>
-          <div className="text-sm text-gray-500">My Description</div>
-          <button className="rounded-2xl w-max ring-1 ring-red-500 text-red-500 text-xs py-2 px-4 hover:bg-red-500 hover:text-white">Add to Cart</button>
-        </Link>
+       
 
-        <Link
-          href="/test"
-          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-        >
-          <div className="relative w-full h-80">
-            <Image
-              src="/products/product1.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-            />
-            <Image
-              src="/products/product2.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md "
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$29.99</span>
-          </div>
-          <div className="text-sm text-gray-500">My Description</div>
-          <button className="rounded-2xl w-max ring-1 ring-red-500 text-red-500 text-xs py-2 px-4 hover:bg-red-500 hover:text-white">Add to Cart</button>
-        </Link>
-
-        <Link
-          href="/test"
-          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-        >
-          <div className="relative w-full h-80">
-            <Image
-              src="/products/product1.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-            />
-            <Image
-              src="/products/product2.png"
-              alt="product"
-              fill
-              sizes="25vw"
-              className="absolute object-cover rounded-md "
-            />
-          </div>
-          <div className="flex justify-between">
-            <span className="font-medium">Product Name</span>
-            <span className="font-semibold">$29.99</span>
-          </div>
-          <div className="text-sm text-gray-500">My Description</div>
-          <button className="rounded-2xl w-max ring-1 ring-red-500 text-red-500 text-xs py-2 px-4 hover:bg-red-500 hover:text-white">Add to Cart</button>
-        </Link>
+       
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import CatagoryList from "./component/home/CatagoryList"
+import NewProducts from "./component/home/NewProducts"
 import ProductList from "./component/home/ProductList"
 import Slider from "./component/home/Slider"
 
@@ -8,6 +10,14 @@ const HomePage = () => {
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
         <ProductList />
+      </div>
+      <div className="mt-24 ">
+        <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">Products Catagory</h1>
+        <CatagoryList />
+      </div>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New Products</h1>
+        <NewProducts />
       </div>
     </div>
   )
